2d867fdd433e5ca93e963378dc5d3cd16fa08749
EslamKamel89 <elsamkamelforex@gmail.com>
2025-03-09 11:22:36 +0200

    Enhance Discussion component layout with topic name and title; improve styling for better presentation

--- resources/js/Components/Forum/Discussion.vue
+++ resources/js/Components/Forum/Discussion.vue
@@ -1,7 +1,17 @@
 <template>
     <div class="mt-4 overflow-hidden bg-white shadow-sm sm:rounded-lg">
-        <div class="p-6 text-gray-900">
-            <div>{{ discussion }}</div>
+        <div class="flex items-center space-x-6 p-6 text-gray-900">
+            <div class="flex-grow">
+                <div class="flex items-center space-x-3">
+                    <div
+                        class="inline-flex items-center rounded-lg bg-gray-100 px-3 py-1 text-sm text-gray-600"
+                    >
+                        {{ discussion.topic.name }}
+                    </div>
+                    <h2 class="text-lg font-medium">{{ discussion.title }}</h2>
+                </div>
+            </div>
+            <div>Avatars</div>
         </div>
     </div>
 </template>
