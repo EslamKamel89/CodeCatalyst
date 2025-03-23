28757d58df01a8643ef8b9e4f8ef68e7ff0fdf85
EslamKamel89 <elsamkamelforex@gmail.com>
2025-03-23 22:59:32 +0200

    Refactor PostComp component to improve layout; add reply button functionality with discussion context

--- resources/js/Components/Forum/PostComp.vue
+++ resources/js/Components/Forum/PostComp.vue
@@ -1,32 +1,46 @@
 <template>
-    <div
-        class="mt-4 flex items-start space-x-3 overflow-hidden bg-white p-6 text-gray-900 shadow-sm sm:rounded-lg"
-    >
-        <div class="w-10 flex-shrink-0" v-if="post.user?.avatar_url">
-            <img
-                :src="post.user.avatar_url"
-                alt="User Avatar"
-                class="h-10 w-10 rounded-full"
-            />
-        </div>
-        <div class="w-full">
-            <div class="">
-                <div class="">
-                    {{ post?.user?.username ?? '[User Deleted]' }}
-                </div>
-                <div class="text-sm text-gray-500">
-                    {{ post.created_at.human }}
-                </div>
+    <div class="mt-4 bg-white p-6 text-gray-900 shadow-sm sm:rounded-lg">
+        <div class="flex items-start space-x-3 overflow-hidden">
+            <div class="w-10 flex-shrink-0" v-if="post.user?.avatar_url">
+                <img
+                    :src="post.user.avatar_url"
+                    alt="User Avatar"
+                    class="h-10 w-10 rounded-full"
+                />
             </div>
-            <div class="markdown mt-3" v-html="post.body_markdown"></div>
+            <div class="w-full">
+                <div class="">
+                    <div class="">
+                        {{ post?.user?.username ?? '[User Deleted]' }}
+                    </div>
+                    <div class="text-sm text-gray-500">
+                        {{ post.created_at.human }}
+                    </div>
+                </div>
+                <div class="markdown mt-3" v-html="post.body_markdown"></div>
+            </div>
+        </div>
+        <div v-if="post.discussion.user_can.reply" class="flex justify-end">
+            <button
+                @click="handleReply"
+                class="btn btn-success btn-sm text-xs text-white"
+            >
+                Reply
+            </button>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import useCreatePost from '@/Composables/useCreatePost';
 import { Post } from '@/types/types';
 
-defineProps<{
+const props = defineProps<{
     post: Post;
 }>();
+const { showForm, setDiscussion } = useCreatePost();
+const handleReply = () => {
+    showForm();
+    setDiscussion(props.post.discussion);
+};
 </script>
