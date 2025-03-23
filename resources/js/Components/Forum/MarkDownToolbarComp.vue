ef306424b31d705f8332643ccaf935b928a551f0
EslamKamel89 <elsamkamelforex@gmail.com>
2025-03-23 09:37:18 +0200

    Refactor Markdown toolbar to use SVG icons; create Svg component for dynamic icon loading

--- resources/js/Components/Forum/MarkDownToolbarComp.vue
+++ resources/js/Components/Forum/MarkDownToolbarComp.vue
@@ -1,20 +1,34 @@
 <template>
     <markdown-toolbar>
         <div class="flex items-center gap-x-3">
-            <md-bold class="toolbar-button">bold</md-bold>
-            <md-header class="toolbar-button">header</md-header>
-            <md-italic class="toolbar-button">italic</md-italic>
-            <md-quote class="toolbar-button">quote</md-quote>
-            <md-code class="toolbar-button">code</md-code>
-            <md-link class="toolbar-button">link</md-link>
+            <md-bold class="toolbar-button" title="bold"
+                ><Svg class="h-7 w-7 object-fill" name="bold"
+            /></md-bold>
+            <md-header class="toolbar-button" title="header">
+                <Svg class="h-7 w-7 object-fill" name="header"
+            /></md-header>
+            <md-italic class="toolbar-button" title="italic"
+                ><Svg class="h-7 w-7 object-fill" name="italic"
+            /></md-italic>
+            <md-quote class="toolbar-button" title="quote"
+                ><Svg class="h-7 w-7 object-fill" name="quote"
+            /></md-quote>
+            <md-code class="toolbar-button" title="code"
+                ><Svg class="h-7 w-7 object-fill" name="code"
+            /></md-code>
+            <md-link class="toolbar-button" title="link"
+                ><Svg class="h-7 w-7 object-fill" name="link"
+            /></md-link>
         </div>
     </markdown-toolbar>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Svg from '../Svg.vue';
+</script>
 
 <style scoped>
 .toolbar-button {
-    @apply cursor-pointer rounded-lg bg-slate-600 px-3 py-2 text-white;
+    @apply flex cursor-pointer items-center justify-center rounded-full bg-slate-600 pl-1 pt-1 text-center text-white;
 }
 </style>
---
+++ resources/js/Components/Svg.vue
@@ -0,0 +1,12 @@
+<template>
+    <div><component :is="icon" /></div>
+</template>
+
+<script setup lang="ts">
+import { defineAsyncComponent } from 'vue';
+
+const props = defineProps<{
+    name: string;
+}>();
+const icon = defineAsyncComponent(() => import(`../../svg/${props.name}.svg`));
+</script>
