3182187f888a9926adcb076c447f1fa6b169b6f3
EslamKamel89 <elsamkamelforex@gmail.com>
2025-03-23 09:01:22 +0200

    Add vite-svg-loader dependency and configure SVG loader in Vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
+import svgLoader from 'vite-svg-loader';
 
 export default defineConfig({
     plugins: [
@@ -20,5 +21,6 @@ export default defineConfig({
                 },
             },
         }),
+        svgLoader({ svgo: false }),
     ],
 });
